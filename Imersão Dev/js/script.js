48202671f04c46cdd159e13350e682e1d62c3d39
Netoamf <tonemfn@gmail.com>
2023-03-22 19:36:11 -0300

    Imersão dev - Marco Bruno dev

---
+++ Imersão Dev/js/script.js
@@ -0,0 +1,17 @@
+
+//JSON
+const curso = {
+    title: "Harry Potter",
+    url: "https://caminho.com/imagem",
+};
+
+//HTML
+
+const img = document.createElement("img");
+const listBanners = document.querySelector(".list-banners")
+
+img.src = "imagens/Curso-JavaScript-Thumbnail.jpg";
+img.alt = "Curso de JavaScrip do Professor Gustavo Guanabara";
+img.classList.add("main-banners");
+
+listBanners.insertAdjacentHTML("beforeend", img);
