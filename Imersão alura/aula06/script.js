70976a614b67f4033c0e673fa322c79005ea12a3
Netoamf <tonemfn@gmail.com>
2023-03-26 19:25:00 -0300

    alura dev dia 6

---
+++ Imersão alura/aula06/script.js
@@ -0,0 +1,37 @@
+var paulo = {
+    nome: "Paulo",
+    vitorias: 2,
+    empates: 5,
+    derrotas: 1,
+    pontos: 0
+}
+
+var rafa ={
+    nome: "Rafa",
+    vitorias: 3,
+    empates:5,
+    derrotas: 2,
+    pontos: 0
+}
+rafa.pontos = calculaPontos(rafa)
+paulo.pontos = calculaPontos(paulo)
+
+function calculaPontos(jogador) {
+    var pontos = (jogador.vitorias *3) + jogador.empates
+    return pontos
+}
+
+var jogadores = [rafa, paulo]
+
+function exibirJogadoresNaTela(jogadores) {
+    var html = ""
+    for (var i = 0; i < jogadores.length; i++) {
+        html += "<tr><td>" +jogadores[i].nome + "</td>"
+        html += "<td>" +jogadores[i].vitorias + "</td>"
+        html += "<td>" +jogadores[i].empates + "</td>"
+        html += "<td>" +jogadores[i].derrotas + "</td>"
+        html += "<td>" +jogadores[i].pontos + "</td></tr>";
+    }
+    var tabelaJogadores = document.getElementById("tabelaJogadores")
+    tabelaJogadores.innerHTML = html
+}
