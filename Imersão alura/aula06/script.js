fbe92ec2281985677ca0bf0b1adc18faecbab4c4
Netoamf <tonemfn@gmail.com>
2023-03-26 20:41:50 -0300

    update js

--- Imersão alura/aula06/script.js
+++ Imersão alura/aula06/script.js
@@ -22,7 +22,7 @@ function calculaPontos(jogador) {
 }
 
 var jogadores = [rafa, paulo]
-
+exibirJogadoresNaTela(jogadores)
 function exibirJogadoresNaTela(jogadores) {
     var html = ""
     for (var i = 0; i < jogadores.length; i++) {
@@ -30,8 +30,32 @@ function exibirJogadoresNaTela(jogadores) {
         html += "<td>" +jogadores[i].vitorias + "</td>"
         html += "<td>" +jogadores[i].empates + "</td>"
         html += "<td>" +jogadores[i].derrotas + "</td>"
-        html += "<td>" +jogadores[i].pontos + "</td></tr>";
+        html += "<td>" +jogadores[i].pontos + "</td>";
+        html += "<td><button onClick='adicionarVitoria(" + i + ")'>Vitória</button></td>"
+        html +="<td><button onClick='adicionarEmpate("+ i +")'>Empate</button></td>"
+        html += "<td><button onClick='adicionarDerrota("+ i +")'>Derrota</button></td></tr>"
     }
     var tabelaJogadores = document.getElementById("tabelaJogadores")
     tabelaJogadores.innerHTML = html
+}
+
+function adicionarVitoria(i) {
+    var jogador = jogadores[i]
+    jogador.vitorias++
+    jogador.pontos = calculaPontos(jogador)
+    exibirJogadoresNaTela(jogadores)
+}
+
+function adicionarEmpate(i) {
+    var jogador = jogadores[i]
+    jogador.empates++
+    jogador.pontos = calculaPontos(jogador)
+    exibirJogadoresNaTela(jogadores)
+}
+
+function adicionarDerrota(i) {
+    var jogador = jogadores[i]
+    jogador.derrotas++
+    jogador.pontos = calculaPontos(jogador)
+    exibirJogadoresNaTela(jogadores)
 }
